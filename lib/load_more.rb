95bcd639d21c05de17dca2eefa0602d02bfbcc39
Niklas Granander <niklas.granander@ek-s.se>
2017-06-27 18:43:37 +0200

    Add capybara for view testing. Include view helpers on ActionView load

--- lib/load_more.rb
+++ lib/load_more.rb
@@ -1,2 +1,7 @@
 require 'load_more/version'
 require 'load_more/engine' if defined?(Rails)
+require 'load_more/action_view'
+
+ActiveSupport.on_load(:action_view) do
+  include LoadMore::ActionView
+end
